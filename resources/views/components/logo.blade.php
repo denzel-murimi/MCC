34d2191e73acae8d668303d16336a191815396a5
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-02-05 21:38:13 +0300

    footer

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/Logo.png') }}" style="width: 100px;" alt="{{ config('app.name') }} Logo" class="logo-image">
+<img src="{{ asset('images/Logo.png') }}" style="width: 200px;" alt="{{ config('app.name') }} Logo" class="logo-image">
--- resources/views/draft2.blade.php
+++ resources/views/draft2.blade.php
@@ -7,7 +7,7 @@
                 <div class="text-center lg:text-left mt-40">
                     <h1 class="font-semibold text-gray-900 text-3xl md:text-6xl leading-normal mb-6">Caring today, <br> shaping tomorrow</h1>
 
-                    <p class="font-light text-gray-400 text-md md:text-lg leading-normal mb-12">We provide care for disabledannel for peoples of <br> worldwide to support people and organizers</p>
+                    <p class="font-light text-gray-400 text-md md:text-lg leading-normal mb-12">We provide care for disabled children and for peoples of <br> worldwide to support people and organizers</p>
 
                     <button class="px-6 py-4 bg-primary-900 font-semibold text-white text-lg rounded-xl hover:bg-secondary-700 transition ease-in-out duration-500">Get started</button>
                 </div>
@@ -18,4 +18,7 @@
             </div>
         </div>
     </section>
+
+    <x-footer></x-footer>
+
 </x-layout>
